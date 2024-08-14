8a023cbbcda037e0856d0343a37056ae4dc48fe6
esapuolakka <esa.puolakka@gmail.com>
2024-08-14 15:17:21 +0300

    initial commit

---
+++ backend/src/main/java/com/example/shareholder/ShareholderApplication.java
@@ -0,0 +1,13 @@
+package com.example.shareholder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareholderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareholderApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/shareholder/ShareholderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shareholder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareholderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
