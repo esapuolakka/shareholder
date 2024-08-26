e2125b8f31637bef149ea42192e397ddad594bc9
SaimaS <saima.salonen@gmail.com>
2024-08-26 14:52:34 +0300

    mobileview menu done

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -7,7 +7,8 @@ import {
   MenuItem,
   Box,
 } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu"; // switched to svg
+import MenuIcon from "@mui/icons-material/Menu"; // Burger icon, switched to svg
+import CloseIcon from "@mui/icons-material/Close"; // X icon
 import { Link } from "react-router-dom";
 import "./reset.css";
 import "./header.css";
@@ -22,6 +23,7 @@ const pages = [
 
 export default function HeaderNavBar() {
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const isMenuOpen = Boolean(anchorEl);
 
   const handleMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
@@ -40,6 +42,13 @@ export default function HeaderNavBar() {
             alt="PHZ Full Stack Logo"
             className="navbar__logo"
           />
+          {/* <Link to="/osakasluettelo">
+            <img
+              src="/phz.png"
+              alt="PHZ Full Stack Logo"
+              className="navbar__logo"
+            />
+          </Link> */}
         </Box>
         {/* descktop */}
         <Box
@@ -55,13 +64,13 @@ export default function HeaderNavBar() {
               {page.label}
             </a>
           ))}
-          {/*  <Link
-//          key={page.label}
-//          to={page.path}
-//          className="navbar-item"
-//           >
-//          {page.label}
-//          </Link> */}
+          {/* <Link
+        key={page.label}
+          to={page.path}
+          className="navbar-item"
+           >
+          {page.label}
+          </Link> */}
         </Box>
         {/* mobile view */}
         <Box sx={{ display: { xs: "flex", md: "none" } }}>
@@ -69,31 +78,34 @@ export default function HeaderNavBar() {
             edge="end"
             color="inherit"
             aria-label="menu"
-            onClick={handleMenuOpen}
+            onClick={isMenuOpen ? handleMenuClose : handleMenuOpen}
             className="navbar-burger"
           >
-            <svg
-              width="25"
-              height="28" // height, including lineheight
-              viewBox="0 0 30 15" // ViewBox for relevant measures
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <rect y="0" width="30" height="2" fill="#ed692f" /> {/* First */}
-              <rect y="7" width="30" height="2" fill="#ed692f" /> {/* Second */}
-              <rect y="14" width="30" height="2" fill="#ed692f" /> {/* Third */}
-            </svg>
+            {isMenuOpen ? (
+              <CloseIcon /> // Display "X" icon
+            ) : (
+              <MenuIcon /> // Display burger icon
+            )}
           </IconButton>
         </Box>
         <Menu
           anchorEl={anchorEl}
           open={Boolean(anchorEl)}
           onClose={handleMenuClose}
-          container={document.body} // Määrittää, että Menu renderöidään suoraan body-elementtiin
           sx={{
-            display: { xs: "block", md: "none" },
+            "& .MuiPaper-root": {
+              boxShadow: "none",
+              width: "100vw",
+              marginTop: "7px",
+              borderRadius: "1px",
+            },
+            "& .MuiList-root": {
+              paddingTop: "0px",
+              paddingBottom: "0px",
+              width: "100vw",
+            },
           }}
-          className="navbar-menu"
+          className="navbar-mobile-menu"
         >
           {pages.map((page) => (
             <MenuItem
@@ -101,9 +113,6 @@ export default function HeaderNavBar() {
               onClick={handleMenuClose}
               component="a"
               href={`#${page.label.toLowerCase().replace(/ /g, "-")}`}
-              sx={{
-                width: "100%", // Varmistaa, että jokainen MenuItem täyttää koko valikon leveyden
-              }}
               className="navbar-item"
             >
               {page.label}
