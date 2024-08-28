7974b9265094d74764be589d7fb007ec25d9ff49
esapuolakka <esa.puolakka@gmail.com>
2024-08-28 20:16:12 +0300

    add getShareholder method to api to connect service class

--- backend/src/main/java/com/example/shareholder/service/ShareholderService.java
+++ backend/src/main/java/com/example/shareholder/service/ShareholderService.java
@@ -1,5 +1,6 @@
 package com.example.shareholder.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import com.example.shareholder.model.Shareholder;
@@ -8,6 +9,7 @@ import com.example.shareholder.repository.ShareholderRepository;
 @Service
 public class ShareholderService {
 
+  @Autowired
   private ShareholderRepository shareholderRepository;
  
   public List<Shareholder> getShareholders() {
