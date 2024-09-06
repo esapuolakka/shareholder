aefe0a39c6ebad3f0d1dd73ddd4a104157414ff5
Narges Ghanbari <narges.ghanbari@gmail.com>
2024-09-06 22:06:00 +0300

    Apply style to line chart

--- frontend/src/components/LineChart.jsx
+++ frontend/src/components/LineChart.jsx
@@ -6,15 +6,24 @@ const options = {
     type: "line",
   },
   title: {
-    text: "My chart",
+    text: "ARVONKEHITYS (valuation history)",
+    align: screenLeft,
   },
   series: [
     {
-      data: [1, 2, 3],
+      data: [4200, 347, 4010, 760, 2260, 1120, 4500],
+      color: "#ed692f",
+      name: "Osakkeen hinta",
+      marker: {
+        radius: 6,
+        lineColor: "#666666",
+        fillColor: "rgb(204 204 204 / 50%)",
+        lineWidth: 1,
+      },
     },
   ],
   xAxis: {
-    tickInterval: 1,
+    categories: [2018, 2019, 2020, 2021, 2022, 2023, 2024],
   },
 };
 
