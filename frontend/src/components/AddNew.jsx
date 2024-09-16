662910ae1ea98b62b98632ae48f3bef3886f2811
SaimaS <saima.salonen@gmail.com>
2024-09-16 19:14:57 +0200

    add placeholders to inputs in lisaa uusi and owner details view. Logo changed to symmetrical one.

--- frontend/src/components/AddNew.jsx
+++ frontend/src/components/AddNew.jsx
@@ -20,51 +20,51 @@ const AddNew = ({ persons }) => {
     <>
       <section className={styles.AddNew}>
         <div className={styles.addBox}>
-          <h2>Osakemyynti</h2>
+          <h2>Osakemyynti ilmoitus</h2>
           <form className={styles.generalItemContainer}>
             <div className={styles.formRow}>
-              <label> Luovuttaja/ Myyjä</label>
+              <label> Luovuttaja/ Myyjä:</label>
               <SelectPerson persons={persons} />
             </div>
             <div className={styles.formRow}>
-              <label>Saaja/ Ostaja</label>
+              <label>Saaja/ Ostaja:</label>
               <SelectPerson persons={persons} />
             </div>
             <div className={styles.inlineFormRow}>
               <div>
-                <label>Saantopäivä</label>
+                <label>Saantopäivä:</label>
                 <input />
               </div>
               <div>
-                <label>Maksupäivä</label>
+                <label>Maksupäivä:</label>
                 <input />
               </div>
             </div>
             <div className={styles.inlineFormRow}>
               <div>
-                <label>Osakenumerot alkaen</label>
+                <label>Osakenumerot alkaen:</label>
                 <input />
               </div>
               <div>
-                <label>Osakenumerot loppuen</label>
+                <label>Osakenumerot loppuen:</label>
                 <input />
               </div>
             </div>
             <div className={styles.formRow}>
-              <label>Osakkeiden määrä</label>
+              <label>Osakkeiden määrä:</label>
               <input />
             </div>
             <div className={styles.formRow}>
-              <label>Hinta per osake</label>
+              <label>Hinta per osake:</label>
               <input />
             </div>
             <div className={styles.formRow}>
-              <label>EUR</label>
+              <label>EUR:</label>
               <input />
             </div>
             <div className={styles.formRow}>
-              <label>Huomioitavaa</label>
-              <input />
+              <label>Huomioitavaa:</label>
+              <textarea className={styles.formRow} rows="7" />
             </div>
             <div className={styles.formRow}>
               <button className={styles.styledButton}>Lisää</button>
@@ -75,31 +75,37 @@ const AddNew = ({ persons }) => {
           <h2>Osakkeenomistaja</h2>
           <form className={styles.generalItemContainer}>
             <div className={styles.formRow}>
-              <label>Osakkaan numero</label>
+              <label>Osakkaan numero:</label>
+              <input placeholder="** next free id number shown here **" />
+            </div>
+            <div className={styles.formRow}>
+              <label>Osakkaan nimi/ Yrityksen nimi:</label>
+              <input placeholder="Etunimi/ Yrityksen nimi" />
+              <input placeholder="Sukunimi" />
+            </div>
+            <div className={styles.formRow}>
+              <label>Henkilötunnus/ Y-tunnus:</label>
               <input />
             </div>
             <div className={styles.formRow}>
-              <label>Henkilötunnus/ Y-tunnus</label>
+              <label>Kotipaikka:</label>
+              <input placeholder="Kaupunki" />
+            </div>
+            <div className={styles.formRow}>
+              <label>Yhteystiedot:</label>
+              <input placeholder="Osoite" />
+              <input placeholder="Postinumero" />
+            </div>
+            <div className={styles.formRow}>
+              <label>Sähköposti:</label>
               <input />
             </div>
             <div className={styles.formRow}>
-              <label>Kotipaikka</label>
+              <label>Puhelinnumero:</label>
               <input />
             </div>
             <div className={styles.formRow}>
-              <label>Yhteystiedot</label>
-              <input />
-            </div>
-            <div className={styles.formRow}>
-              <label>Sähköposti</label>
-              <input />
-            </div>
-            <div className={styles.formRow}>
-              <label>Puhelinnumero</label>
-              <input />
-            </div>
-            <div className={styles.formRow}>
-              <label>Tilinumero</label>
+              <label>Tilinumero:</label>
               <input />
             </div>
             <div className={styles.formRow}>
