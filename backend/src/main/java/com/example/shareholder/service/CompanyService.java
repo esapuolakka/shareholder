48eeaac49459cd47bd80520399dcc0b929bafadb
Esa Puolakka <130772020+esapuolakka@users.noreply.github.com>
2024-10-07 17:21:49 +0300

    fix update logic and remove useless

--- backend/src/main/java/com/example/shareholder/service/CompanyService.java
+++ backend/src/main/java/com/example/shareholder/service/CompanyService.java
@@ -2,8 +2,7 @@ package com.example.shareholder.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import java.util.Optional;
 import com.example.shareholder.model.Company;
 import com.example.shareholder.repository.CompanyRepository;
 
@@ -13,35 +12,27 @@ public class CompanyService {
     @Autowired
     private CompanyRepository companyRepository;
 
-    public List<Company> getCompanies() {
-        return companyRepository.findAll();
+    public Company getCompany() {
+        return companyRepository.findById(1L)
+                .orElseThrow(() -> new IllegalArgumentException("Yhtiötä ei löytynyt"));
     }
 
-    public Company getCompanyById(Long id) {
-        return companyRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Yhtiötä ei löytynyt id:llä " + id));
-    }
+    public Company updateCompany(Company company) {
+        Optional<Company> companyOptional = companyRepository.findById(1L);
 
-    public Company addCompany(Company company) {
-        return companyRepository.save(company);
-    }
-
-    public Company updateCompany(Long id, Company company) {
-        Company existingCompany = companyRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Yhtiötä ei löytynyt " + id));
-
-        existingCompany.setName(company.getName());
-        existingCompany.setCompanyId(company.getCompanyId());
-        existingCompany.setCity(company.getCity());
-        existingCompany.setUrl(company.getUrl());
-
-        return companyRepository.save(existingCompany);
-    }
-
-    public void deleteCompany(Long id) {
-        companyRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Yhtiötä ei löytynyt id:llä " + id));
-
-        companyRepository.deleteById(id);
+        if (companyOptional.isPresent()) {
+            Company existingCompany = companyOptional.get();
+            existingCompany.setName(company.getName());
+            existingCompany.setCompanyId(company.getCompanyId());
+            existingCompany.setCity(company.getCity());
+            existingCompany.setUrl(company.getUrl());
+            return companyRepository.save(existingCompany);
+        } else {
+            Company newCompany = new Company(company.getName(),
+                    company.getCompanyId(),
+                    company.getCity(),
+                    company.getUrl());
+            return companyRepository.save(newCompany);
+        }
     }
 }
